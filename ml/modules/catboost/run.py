27f811b93a581ceb6fa02d880741031d87efbf9a
matyahiko <sanakan.ver3@gmail.com>
2025-02-26 08:18:22 +0000

    データリーク修正

--- ml/modules/catboost/run.py
+++ ml/modules/catboost/run.py
@@ -4,6 +4,7 @@ import sys
 import pandas as pd
 
 from modules.catboost.catboost_classifier_and_regressor import CatBoostClassifierAndMultiRegressor
+from modules.catboost.catboost_regressor import CatBoostMultiRegressor
 from modules.catboost.plot_metrics import plot_metrics
 
 
@@ -14,15 +15,15 @@ def load_data(filename: str, data_dir: str, target_columns: list, num_folds: int
     else:
         train = pd.read_pickle(os.path.join(data_dir, f"{filename}_train.pkl"))
         test = pd.read_pickle(os.path.join(data_dir, f"{filename}_test.pkl"))
-
-    # 元のDataFrameをそのまま x_train, x_test として返す
-    x_train = train.copy()
-    x_test = test.copy()
-
+    
     # ターゲットデータは別途抽出しておく
     y_train = train[target_columns].copy()
     y_test = test[target_columns].copy()
-
+    
+    # ターゲット列を削除した特徴量データを作成
+    x_train = train.drop(columns=target_columns).copy()
+    x_test = test.drop(columns=target_columns).copy()
+    
     return x_train, y_train, x_test, y_test
 
 def run(config: dict):
@@ -31,16 +32,28 @@ def run(config: dict):
     data_dir = config["data_dir"]
     target_columns = config["target_columns"]
     do_hyperparam_search = config.get("do_hyperparam_search", False)
+    two_stage = config.get("two_stage", False)
     
     
-    if do_hyperparam_search:
-        pass
+    if two_stage:
+        if do_hyperparam_search:
+            pass
+        else:
+            x_train, y_train, x_test, y_test = load_data(filename, data_dir, target_columns)
+            model = CatBoostClassifierAndMultiRegressor(target_columns=target_columns)
+            model.train(x_train, y_train, x_test, y_test)
+            metrics = model.evaluate(x_test, y_test)
+            model.save_models()
+            plot_metrics(metrics, save_dir="plots/catboost")
     else:
-        x_train, y_train, x_test, y_test = load_data(filename, data_dir, target_columns)
-        model = CatBoostClassifierAndMultiRegressor(target_columns=target_columns)
-        model.train(x_train, y_train, x_test, y_test)
-        metrics = model.evaluate(x_test, y_test)
-        model.save_models()
-        plot_metrics(metrics, save_dir="plots/catboost")
+        if do_hyperparam_search:
+            pass
+        else:
+            x_train, y_train, x_test, y_test = load_data(filename, data_dir, target_columns)
+            model = CatBoostMultiRegressor(target_columns=target_columns)
+            model.train(x_train, y_train, x_test, y_test)
+            metrics = model.evaluate(x_test, y_test)
+            model.save_models()
+            plot_metrics(metrics, save_dir="plots/catboost")
         
         
